321619caba288d9cbf78877651bda864d6cb746c
Kat3110 <kat3110@mail.ru>
2022-01-21 01:30:24 +0300

    animation

--- script.js
+++
@@ -1,6 +0,0 @@
-// var docStyle = document.documentElement.style;
-
-// document.addEventListener('mousemove', function(e) {
-//   docStyle.setProperty('--mouse-x', e.clientX);
-//   docStyle.setProperty('--mouse-y', e.clientY);
-// });
